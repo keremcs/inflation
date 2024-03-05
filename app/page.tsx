ae505ea626160d6bcae972fc1bd44c30ea238667
keremcs <147047280+keremcs@users.noreply.github.com>
2024-03-05 15:41:05 +0300

    money growth online

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,117 @@
-import CBGame from "@/components/cbgame";
+import { cookies, headers } from "next/headers";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/types/supabase";
+import { Input } from "@/components/ui/input";
+import { LoadingButton } from "@/components/loading-button";
+import { redirect } from "next/navigation";
+import { z } from "zod";
 import Hamburger from "@/components/hamburger";
+import dynamic from "next/dynamic";
 
-export default function Home() {
+const DNGame = dynamic(() => import("@/components/cbgame"), { ssr: false });
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
+  const ip = headers().get("x-real-ip") ?? "95.183.240.91";
+  const username = cookies().get("username")?.value ?? "dn";
+
+  const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PRIVATE_SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
+        detectSessionInUrl: false,
+      },
+    }
+  );
+
+  const { data: player, error: playerError } = await supabase
+    .from("cbgame")
+    .select("id, game")
+    .eq("ip", ip)
+    .eq("username", username);
+  if (playerError) {
+    redirect(`/?message=${playerError.message}`);
+  }
+
+  async function setUsername(formData: FormData) {
+    "use server";
+
+    const uchema = z.object({
+      username: z
+        .string()
+        .min(3)
+        .max(17)
+        .regex(/^[a-zA-Z0-9]+$/),
+    });
+    const uparsed = uchema.safeParse({
+      username: formData.get("username"),
+    });
+    if (!uparsed.success) {
+      redirect("/?message=Invalid%20username");
+    }
+
+    const supabaseUction = createClient<Database>(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PRIVATE_SUPABASE_SERVICE_ROLE_KEY!,
+      {
+        auth: {
+          persistSession: false,
+          autoRefreshToken: false,
+          detectSessionInUrl: false,
+        },
+      }
+    );
+
+    const { error: newError } = await supabaseUction.from("cbgame").insert({
+      ip,
+      username: uparsed.data.username,
+      game: 0,
+      s1: 0,
+      s2: 0,
+    });
+    if (newError) {
+      if (newError.code === "23505") {
+        redirect("/?message=Username%20taken");
+      }
+      redirect(`/?message=${newError.message}`);
+    }
+
+    cookies().set("username", uparsed.data.username);
+
+    redirect("/");
+  }
+
+  if (player.length === 0) {
+    return (
+      <main className="min-h-screen flex flex-col justify-center items-center p-12">
+        <form className="flex flex-col gap-6" action={setUsername}>
+          <div className="flex justify-center">Enter your username</div>
+          <Input
+            type="text"
+            name="username"
+            className="text-sm"
+            placeholder="Username"
+            pattern="[a-zA-Z0-9]{3,17}"
+            required
+          />
+          <LoadingButton />
+          {searchParams?.message && (
+            <p className="text-red-500 text-center">{searchParams.message}</p>
+          )}
+        </form>
+      </main>
+    );
+  }
+
+  const playerId = player[0].id;
+  const playerGame = player[0].game;
+
   return (
     <main className="min-h-screen flex flex-col">
       <div className="flex flex-row justify-center border-b h-[57px]">
@@ -15,8 +125,11 @@ export default function Home() {
           <div className="flex w-[90px] justify-end"></div>
         </div>
       </div>
-      <div className="flex grow justify-center">
-        <CBGame />
+      <div className="flex flex-col grow justify-center gap-6">
+        <DNGame uid={playerId} game={playerGame} />
+        {searchParams?.message && (
+          <p className="text-red-500 text-center">{searchParams.message}</p>
+        )}
       </div>
     </main>
   );
--- components/cbsend.ts
+++ components/cbsend.ts
@@ -13,6 +13,7 @@ export async function cbsend(
     i2: number;
     i3: number;
     i4: number;
+    o0: number;
     o1: number;
     o2: number;
     o3: number;
@@ -36,11 +37,11 @@ export async function cbsend(
     }
   );
 
-  function formula(r: number, ib: number, ia: number, oa: number) {
-    const og = ib - r + 1;
-    const inf = ib + og;
-    const to = parseFloat(og.toFixed(2));
+  function formula(m: number, ib: number, ia: number, ob: number, oa: number) {
+    const inf = (0.25 * ib + 0.75 * (2 * m + ob)) / 1.75;
+    const og = m - inf + ob;
     const ti = parseFloat(inf.toFixed(2));
+    const to = parseFloat(og.toFixed(2));
     if (to === oa && ti === ia) return true;
     return false;
   }
@@ -64,10 +65,10 @@ export async function cbsend(
   };
 
   if (
-    formula(data.r1, data.i0, data.i1, data.o1) &&
-    formula(data.r2, data.i1, data.i2, data.o2) &&
-    formula(data.r3, data.i2, data.i3, data.o3) &&
-    formula(data.r4, data.i3, data.i4, data.o4)
+    formula(data.r1, data.i0, data.i1, data.o0, data.o1) &&
+    formula(data.r2, data.i1, data.i2, data.o1, data.o2) &&
+    formula(data.r3, data.i2, data.i3, data.o2, data.o3) &&
+    formula(data.r4, data.i3, data.i4, data.o3, data.o4)
   ) {
     if (done === data.s) {
       const { error } = await supabase
